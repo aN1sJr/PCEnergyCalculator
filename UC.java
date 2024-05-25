894f81ff85c162b52e56a8543897500e4685832a
Lakardi Mohamed Anis <127695733+lakardi-mohamed-anis@users.noreply.github.com>
2024-05-25 16:28:42 +0100

    uc class

---
+++ UC.java
@@ -0,0 +1,11 @@
+public class UC extends  PC{
+    String CPU;
+    int RAM;
+    double puissance;
+
+    UC (String CPU, int RAM, double puissance){
+        this.CPU = CPU;
+        this.RAM = RAM;
+        this.puissance = puissance;
+    }
+}
